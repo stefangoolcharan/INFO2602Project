718862585efda2f6fe4741b80f4358677e249c0f
Stefan Goolcharan <60503198+stefangoolcharan@users.noreply.github.com>
2022-04-23 10:30:44 -0800

    Add files via upload

---
+++ src/components/Rating.js
@@ -0,0 +1,28 @@
+const Rating ={
+    render:(props) => {
+        if(!props.value){
+            return `<div></div>`
+        }
+        return `
+        <div class = "rating">
+            <span>
+                <i class = "${props.value>=1? 'fa fa-star': props.value>=0.5? 'fa fa-star-half-o': ' fa fa-start-o'}"></i>
+            </span>
+            <span>
+                <i class = "${props.value>=2? 'fa fa-star': props.value>=1.5? 'fa fa-star-half-o': ' fa fa-start-o'}"></i>
+            </span>
+            <span>
+                <i class = "${props.value>=3? 'fa fa-star': props.value>=2.5? 'fa fa-star-half-o': ' fa fa-start-o'}"></i>
+            </span>
+            <span>
+                <i class = "${props.value>=4? 'fa fa-star': props.value>=3.5? 'fa fa-star-half-o': ' fa fa-start-o'}"></i>
+            </span>
+            <span>
+                <i class = "${props.value>=5? 'fa fa-star': props.value>=4.5? 'fa fa-star-half-o': ' fa fa-start-o'}"></i>
+            </span>
+            <span>${props.text|| ''}</span>
+        </div>
+        `;
+    },
+}
+export default Rating
